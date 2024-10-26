681f06cadceaa6af80b73873c7825ea308d79abf
mehmetaybey <mehmet.aybey@ogr.iu.edu.tr>
2024-10-26 23:02:17 +0300

    Login Page created

--- BlazingQuiz.Shared/DTOs/LoginDto.cs
+++ BlazingQuiz.Shared/DTOs/LoginDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazingQuiz.Shared.DTOs;
 
 public class LoginDto
 {
+    [Required,EmailAddress,DataType(DataType.EmailAddress)]
     public string UserName { get; set; }
+    [Required,DataType(DataType.Password)]
     public string Password { get; set; }
     
 }
