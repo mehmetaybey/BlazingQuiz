d9c4ff5acf7a626ee2184d36b6a99bc8d6a64f34
mehmetaybey <mehmet.aybey@ogr.iu.edu.tr>
2024-11-27 12:43:43 +0300

    Part 32 of 41 - Loader Implementation, Common CSS, Mobile Navigation UI

--- BlazingQuiz.Mobile/MauiProgram.cs
+++ BlazingQuiz.Mobile/MauiProgram.cs
@@ -6,58 +6,89 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Logging;
 using Refit;
 
-namespace BlazingQuiz.Mobile
-{
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+#if ANDROID
+using System.Net.Security;
+using Xamarin.Android.Net;
+using System.Security.Cryptography.X509Certificates;
 
-            builder.Services.AddMauiBlazorWebView();
+#elif IOS
+using Security;
+
+#endif
+
+
+namespace BlazingQuiz.Mobile;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts => { fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); });
+
+        builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
 #endif
-            builder.Services.AddCascadingAuthenticationState(); 
-            builder.Services.AddSingleton<QuizAuthStateProvider>();
-            builder.Services.AddSingleton<AuthenticationStateProvider>(p => p.GetRequiredService<QuizAuthStateProvider>());
-            builder.Services.AddAuthorizationCore();
+        builder.Services.AddCascadingAuthenticationState();
+        builder.Services.AddSingleton<QuizAuthStateProvider>();
+        builder.Services.AddSingleton<AuthenticationStateProvider>(p => p.GetRequiredService<QuizAuthStateProvider>());
+        builder.Services.AddAuthorizationCore();
 
-            builder.Services.AddSingleton<IStorageService, StorageService>()
-                .AddSingleton<IAppState,AppState>();
-            
-            ConfigureRefit(builder.Services);
-            
-            return builder.Build();
-        }
-        static void ConfigureRefit(IServiceCollection service)
+        builder.Services.AddSingleton<IStorageService, StorageService>()
+            .AddSingleton<IAppState, AppState>();
+
+        ConfigureRefit(builder.Services);
+
+        return builder.Build();
+    }
+
+    private static readonly string ApiBaseUrl = DeviceInfo.Platform == DevicePlatform.Android
+        ? "https://10.0.2.2:7189"
+        : "https://localhost:7189";
+
+    static void ConfigureRefit(IServiceCollection service)
+    {
+
+        service.AddRefitClient<IAuthApi>(GetRefitSettings)
+            .ConfigureHttpClient(SetHttpClient);
+        static void SetHttpClient(HttpClient httpClient) => httpClient.BaseAddress = new Uri(ApiBaseUrl);
+
+
+
+        static RefitSettings GetRefitSettings(IServiceProvider sp)
         {
-            const string baseUrl = "https://localhost:7189";
-            static void SetHttpClient(HttpClient httpClient) => httpClient.BaseAddress = new Uri(baseUrl);
+            var authStateProvider = sp.GetRequiredService<QuizAuthStateProvider>();
 
-            service.AddRefitClient<IAuthApi>()
-                .ConfigureHttpClient(SetHttpClient);
-            
-
-            static RefitSettings GetRefitSettings(IServiceProvider sp)
+            return new RefitSettings
             {
-                var authStateProvider = sp.GetRequiredService<QuizAuthStateProvider>();
-
-                return new RefitSettings
+                AuthorizationHeaderValueGetter = (_, __) => Task.FromResult(authStateProvider.User?.Token ?? ""),
+                HttpMessageHandlerFactory = () =>
                 {
-                    AuthorizationHeaderValueGetter = (_,__) =>Task.FromResult(authStateProvider.User?.Token ?? "")
-                };
-            }
-    
-        }
+ #if ANDROID          
+                    var androidMessageHandler = new AndroidMessageHandler();
+                    androidMessageHandler.ServerCertificateCustomValidationCallback
+                        = (HttpRequestMessage requestMessage, X509Certificate2 certificate2, X509Chain? chain,
+                                SslPolicyErrors sslPolicyErrors)
+                            => certificate2.Issuer == "CN=localhost" || sslPolicyErrors == SslPolicyErrors.None;
 
+                    return androidMessageHandler;
+#elif  IOS
+                    var nsUrlSessionHandler = new NSUrlSessionHandler
+                    {
+                        TrustOverrideForUrl = (NSUrlSessionHandler sender, string url, SecTrust trust) =>
+                                url.StartsWith("https//localhost")
+                    };
+                    return nsUrlSessionHandler;
+                   
+#endif
+                    return null;
+                }
+            };
+        }
     }
 }
